036999cf94154e3c4a48c3260a3d377967c04c2a
Igor Limeira <igor.limeira@outlook.com>
2023-01-23 23:42:32 -0300

    first commit

---
+++ src/test/java/br/com/vr/vrminiautorizador/VrMiniAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vr.vrminiautorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrMiniAutorizadorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
